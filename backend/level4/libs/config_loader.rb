a9d3ab8ae34f71b5a5b5bb3f8662649612d5e915
Clément DENIS <clsined@outlook.com>
2019-05-23 23:02:09 +0200

    Realisation level 4

---
+++ backend/level4/libs/config_loader.rb
@@ -0,0 +1,57 @@
+require 'singleton'
+require 'yaml'
+
+class ConfigLoader
+  include Singleton
+
+  attr_accessor :configs, :is_loaded, :config_file
+
+  def initialize
+    @is_loaded = false
+    @config_file = './conf/config.yml'
+  end
+
+  def configs
+    unless ConfigLoader.instance.is_loaded then
+      ConfigLoader.instance.load
+    end
+    @configs
+  end
+
+  def load
+    unless @is_loaded then
+      yml_conf = YAML::load_file(@config_file)
+      @configs = ConfigFile.new(yml_conf['json_file'], yml_conf['output_json_file'], yml_conf['logger_file'])
+      @is_loaded = true
+      CoreLogger.instance.logger.info("ConfigLoader - load") { "Chargement du fichier de configuration : '#{config_file}'" }
+    end
+  end
+end
+
+class ConfigFile
+  attr :json_file, :output_json_file, :logger_file
+
+  def initialize(json_file, output_json_file, logger_file)
+    @json_file = json_file
+    @output_json_file = output_json_file
+    @logger_file = logger_file
+  end
+
+  def json_file
+    raise ConfigLoaderException.new("ConfigLoader - json_file : Attr 'json_file' not initialized") if @json_file.nil? or @json_file.empty?
+    @json_file
+  end
+
+  def output_json_file
+    raise ConfigLoaderException.new("ConfigLoader - output_json_file : Attr 'output_json_file' not initialized") if @output_json_file.nil? or @output_json_file.empty?
+    @output_json_file
+  end
+
+  def logger_file
+    raise ConfigLoaderException.new("ConfigLoader - logger_file : Attr 'logger_file' not initialized") if @logger_file.nil? or @logger_file.empty?
+    @logger_file
+  end
+end
+
+class ConfigLoaderException < StandardError
+end
